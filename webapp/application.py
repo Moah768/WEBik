4cfa90d620721095ac3ff394c44385f1bfb6a949
Bibi Kok <bibi.kok@live.nl>
2018-01-26 14:18:24 +0000

    unfollow knop bijgevoegd

--- webapp/application.py
+++ webapp/application.py
@@ -463,8 +463,21 @@ def delete():
     # get name of file you want to delete
     delete_name = request.args.get('filename')
 
-    # delte file in database
-    delete.photo = db.execute("DELETE FROM user_uploads WHERE filename = :filename",filename = delete_name)
+    # delete file in database
+    delete_photo = db.execute("DELETE FROM user_uploads WHERE filename = :filename",filename = delete_name)
 
     return redirect(url_for("index"))
+
+
+####### unfollow
+@app.route("/remove_following", methods=["GET", "POST"])
+@login_required
+def remove_following():
+
+    following_username = request.args.get('username')
+
+    remove_following = db.execute("DELETE FROM volgend WHERE own_id = :own_id AND following_username = :following_username", own_id = session["user_id"], following_username = following_username )
+
+    return redirect(url_for("following"))
+
 
